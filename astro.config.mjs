9b38c1d3b05a8f28d0191264496be7f57e43130c
walidelkad <walid.el.kaddouri@student.howest.be>
2025-11-19 12:05:24 +0100

    changed base name to repo name

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://walidelkad.github.io',
-  base: '/cck/',
+  base: '/astrodeploy/',
+  /* base always needs to be name of repository in github same as link*/
 });
